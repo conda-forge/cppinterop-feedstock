34df7a11fb3ad4de609ab2e3a31bb24e548277be
Vassil Vassilev <v.g.vassilev@gmail.com>
2024-03-02 17:30:06 +0000

    Enable windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 rem # Common settings
 
 set CPU_COUNT=%NUMBER_OF_PROCESSORS%
-set PATH="%PATH%;%BUILD_PREFIX%/x86_64-conda-linux-gnu/lib;%BUILD_PREFIX/lib;%PREFIX/lib"
+set PATH="%PREFIX%\Library\bin;%PATH%;%BUILD_PREFIX%/x86_64-conda-linux-gnu/lib;%BUILD_PREFIX/lib;%PREFIX/lib"
 set INCLUDE="%INCLUDE%;%cd%/include;%BUILD_PREFIX/x86_64-conda-linux-gnu/include/c++/12.3.0;%BUILD_PREFIX%/x86_64-conda-linux-gnu/include/c++/12.3.0/x86_64-conda-linux-gnu;%BUILD_PREFIX%/x86_64-conda-linux-gnu/sysroot/usr/include;%BUILD_PREFIX%/include;%PREFIX%/include"
 
 rem ### Build CppInterOp next to llvm-project.
