c8ced74dbc8fc92785c37d4ebb86f16694786298
Alexander Penev <7923188+alexander-penev@users.noreply.github.com>
2024-03-03 09:03:37 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,14 +3,16 @@
 rem # Common settings
 
 set CPU_COUNT=%NUMBER_OF_PROCESSORS%
-set PATH="%BUILD_PREFIX%/Library/bin;%PREFIX%/Library/bin;%PATH%;%BUILD_PREFIX%/x86_64-conda-linux-gnu/lib;%BUILD_PREFIX%/lib;%PREFIX%/lib"
-set INCLUDE="%INCLUDE%;%cd%/include;%BUILD_PREFIX%/x86_64-conda-linux-gnu/include/c++/12.3.0;%BUILD_PREFIX%/x86_64-conda-linux-gnu/include/c++/12.3.0/x86_64-conda-linux-gnu;%BUILD_PREFIX%/x86_64-conda-linux-gnu/sysroot/usr/include;%BUILD_PREFIX%/include;%PREFIX%/include"
+set PATH="%BUILD_PREFIX%\Library\bin;%PREFIX%\Library\bin;%PATH%;%BUILD_PREFIX%\x86_64-conda-linux-gnu\lib;%BUILD_PREFIX%\lib;%PREFIX%\lib"
+set INCLUDE="%INCLUDE%;%cd%\include;%BUILD_PREFIX%\x86_64-conda-linux-gnu\include\c++\12.3.0;%BUILD_PREFIX%\x86_64-conda-linux-gnu\include\c++\12.3.0\x86_64-conda-linux-gnu;%BUILD_PREFIX%\x86_64-conda-linux-gnu\sysroot\usr\include;%BUILD_PREFIX%\include;%PREFIX%\include"
 
 rem ### Build CppInterOp next to llvm-project.
 
 pushd cppinterop
 mkdir build
 cd build
+
+path
 
 cmake ^
   -G "Ninja" ^
