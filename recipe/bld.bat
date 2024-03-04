6860469d4f77354f7085cbea00650d2f82b33921
Vassil Vassilev <v.g.vassilev@gmail.com>
2024-03-04 09:35:08 +0000

    cleanup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,22 +1,10 @@
 setlocal EnableDelayedExpansion
 
-@echo on
-
-rem # Common settings
-
 set CPU_COUNT=%NUMBER_OF_PROCESSORS%
-::set PATH="%BUILD_PREFIX%\Library\bin;%PREFIX%\Library\bin;%PATH%;%BUILD_PREFIX%\x86_64-conda-linux-gnu\lib;%BUILD_PREFIX%\lib;%PREFIX%\lib"
-::set INCLUDE="%INCLUDE%;%cd%\include;%BUILD_PREFIX%\x86_64-conda-linux-gnu\include\c++\12.3.0;%BUILD_PREFIX%\x86_64-conda-linux-gnu\include\c++\12.3.0\x86_64-conda-linux-gnu;%BUILD_PREFIX%\x86_64-conda-linux-gnu\sysroot\usr\include;%BUILD_PREFIX%\include;%PREFIX%\include"
-
-rem ### Build CppInterOp next to llvm-project.
-
-::conda activate %BUILD_PREFIX%
 
 pushd cppinterop
 mkdir build
 cd build
-
-path
 
 cmake ^
   -G "Ninja" ^
@@ -25,8 +13,7 @@ cmake ^
   -DUSE_REPL=ON                             ^
   -DBUILD_SHARED_LIBS=ON                    ^
   -DCPPINTEROP_ENABLE_TESTING=ON            ^
-  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%   ^
-  --trace-expand ..
+  ..
 
 if errorlevel 1 exit 1
 
