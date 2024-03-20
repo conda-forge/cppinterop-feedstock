cbb0a587fa8c46e5c05f5643b969f122847ff173
Vassil Vassilev <v.g.vassilev@gmail.com>
2024-03-20 07:08:13 +0000

    Adjust

--- recipe/test.cpp
+++ recipe/test.cpp
@@ -21,7 +21,7 @@ inline std::string append_path(const std::string &to, const std::string& what) {
 
 using Args = std::vector<std::string>;
 
-void* createInterpreter(const Args &ExtraArgs = {}) {
+void* createInterpreter(const char* CxxCompiler, const Args &ExtraArgs = {}) {
   Args ClangArgs;
   if (std::find(ExtraArgs.begin(), ExtraArgs.end(), "-resource-dir") != ExtraArgs.end()) {
     std::string resource_dir = Cpp::DetectResourceDir();
@@ -31,12 +31,11 @@ void* createInterpreter(const Args &ExtraArgs = {}) {
     ClangArgs.push_back(resource_dir);
   }
   std::vector<std::string> CxxSystemIncludes;
-  const char* DefaultCxxName = "x86_64-conda-linux-gnu-c++";
   // Oddly, conda supports "pseudo cross-compilation" and gives us the compiler
   // include paths relative to the compiler binary. Now we need to find out
   // where that binary is...
   const char* Prefix = CONDA_PREFIX;
-  Cpp::DetectSystemCompilerIncludePaths(CxxSystemIncludes, DefaultCxxName);
+  Cpp::DetectSystemCompilerIncludePaths(CxxSystemIncludes, CxxCompiler);
   for (std::string CxxInclude : CxxSystemIncludes) {
     if (!file_exists(CxxInclude)) {
       // Try make these paths absolute.
@@ -59,18 +58,14 @@ void* createInterpreter(const Args &ExtraArgs = {}) {
   std::transform(ClangArgs.begin(), ClangArgs.end(), Args.begin(),
                  [](const std::string &s) -> const char * { return s.data(); });
 
-  for (const char* arg : Args)
-    std::cout << arg << "\n";
-
-  std::cout << "----------------\n";
-
-  for (auto arg : ClangArgs)
-    std::cout << arg << "\n";
-
   return Cpp::CreateInterpreter(Args/*, {"-cuda"}*/);
 }
 
-int main() {
-  createInterpreter({"-v"});
+int main(int argc, char *argv[]) {
+  if (argc < 2)
+    std::cerr << "Not enough arguments. Pass a compiler name.\n";
+
+  createInterpreter(argv[1], {"-v"});
+
   return Cpp::Process("#include <string>");
 }
